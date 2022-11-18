486baad838f9116941b0ea1e0d369c82d8814f1b
Paul_Trinitas <hyunjai.kim@novustec.co.kr>
2022-11-18 12:54:53 +0900

    Novustec_Homepage_upload

---
+++ Templates/products_trinitas2_modi.dwt.php
@@ -0,0 +1,192 @@
+<?
+include_once "../public_html/_hd.php";
+?>
+
+<style type="text/css">
+#features dd,#features dt{display:none}
+</style>
+
+<div id="p_trinitas2" class="wrap">
+
+    
+    <?
+	include_once "../public_html/_gnb.php";
+	?>
+    
+    
+  <div id="bd">
+    	<div id="sub_title_wrap">
+        	<div id="blue_bar"></div>
+            
+            <div id="sub_title">
+            	<h3><img src="../public_html/inc/img/sub_title_products_trinitas2.png" alt="Products > Trinitas T2" /></h3>
+                <p>TRINITAS T2 커피 로스터는 2006년부터 계속되어 온 고집스런 장인정신과 기술력을 바탕으로 개발되었으며,<br />그 동안 Caffeol Test Lab과 본사 R&D센터를 통한 지속적인 Cupping과 데이터의 축적을 통해 완성되었습니다.</p>
+            </div>
+        </div><!--#sub_title_wrap-->
+        
+        
+        
+    <div class="inner_wrap">
+        	<div class="left"></div>
+      <div class="right">
+           	  <div class="section" id="features">
+                	<h4><strong>Technical Features</strong></h4>
+                    <dl>
+                    	<dt><strong>로스팅 시간</strong></dt>
+                        <dd class="value"><strong>8-10분</strong></dd>
+                        <dd class="icon"><strong><img src="../public_html/inc/img/products_trinitas2_icon_0.png" alt="" /></strong></dd>
+                    </dl>
+                    <dl>
+                    	<dt><strong>로스팅 용량</strong></dt>
+                        <dd class="value"><strong>2kg/batch</strong></dd>
+                        <dd class="icon"><strong><img src="../public_html/inc/img/products_trinitas2_icon_1.png" alt="" /></strong></dd>
+                    </dl>
+                    <dl>
+                    	<dt><strong>열원 타입</strong></dt>
+                        <dd class="value"><strong>LPG / LNG</strong></dd>
+                        <dd class="icon"><strong><img src="../public_html/inc/img/products_trinitas2_icon_2.png" alt="" /></strong></dd>
+                    </dl>
+                    <dl>
+                    	<dt><strong>시간당 생산량</strong></dt>
+                        <dd class="value"><strong>10kg</strong></dd>
+                        <dd class="icon"><strong><img src="../public_html/inc/img/products_trinitas2_icon_3.png" alt="" /></strong></dd>
+                    </dl>
+           	  </div>
+           	  <strong><!--.section--><!--.section--><!--.section--><!--.section--><!--.section--><!--.section--></strong>
+              
+		  <table width="792" height="580" border="0" align="right" id="inform">
+                <tbody>
+                  <tr>
+                    <th width="786" height="110" bgcolor="#FFFFFF" id="info_5"><table width="649" height="580" border="0" align="right" id="inform2">
+                      <tbody>
+                      <tr>
+                        <th height="110" bgcolor="#FFFFFF" id="info_"><table width="649" height="580" border="0" align="right" id="inform3">
+                          <tbody>
+                            <tr>
+                              <th height="110" bgcolor="#FFFFFF" id="info_2"><table width="649" height="1010" border="0" align="right" id="inform4">
+                                <tbody>
+                                  <tr>
+                                    <th height="110" bgcolor="#FFFFFF" id="info_3"><strong>트리니타스 T2는 안정된 열량을 공급하고 유지하도록 설계되어 투입온도, 투입열량, 배출온도 이 세가지만 결정되면 사용자가 구축한 프로파일을 일정하게 재현합니다.</strong></th>
+                                  </tr>
+                                  <tr>
+                                    <th height="110" bgcolor="#FFFFFF" id="info_4"><strong>사용자는 좋은 생두를 선택하고 그 결과물을 맛보는 것에 집중하십시오. 성공적인 로스팅 타겟에 안착하기 위한 노력은 저희가 하겠습니다.</strong></th>
+                                  </tr>
+                                  <tr>
+                                    <th width="643" height="110" bgcolor="#FFFFFF" id="info_1"><p align="left"><strong><span class="desc">로스팅이 진행되며 구간에 따라 사용자가 조작하는 복잡한 과정 없이도 좋은 결과물을 얻을 수 있습니다. 투입한 생두의 종류와 양에 따른 투입온도, 초기투입열량만 결정되면 배출까지 열량과 댐퍼의 조절없이 원활한 진행이 가능합니다. 
+                                      로스팅과정의 복잡성으로 고민하지 마십시오.</span></strong></p></th>
+                                  </tr>
+                                  <tr>
+                                    <th height="110" bgcolor="#FFFFFF" id="info_6"><strong>풍부한 열량과 강력한 냉각으로 시간당 최대 10Kg의 원두를 생산할 수 있습니다. 트리니타스 T2는 2개의 구동모터를 이용하여 드럼과 냉각교반을 작동하고 2개의 송풍모터를 이용하여 배기와 냉각을 합니다. 총 4개의 독립모터가 빠른 연속배치를 가능하게 합니다</strong></th>
+                                  </tr>
+                                  <tr>
+                                    <th height="54" bgcolor="#FFFFFF" id="info_7"><p><strong>트리니타스 T2는 심플하고 직관적인 구조로 체프의 배출과 배기관에 발생하는 슬러지의 청소가 매우 용이합니다. </strong></p></th>
+                                  </tr>
+                                  <tr>
+                                    <th height="54" bgcolor="#FFFFFF" id="info_11">혁신적인 시스템을 채용하여 로스팅 중 체프통을 비울 수 있으며 대용량 체프통을 사용하여 약 12회의 연속 로스팅이 가능합니다.</th>
+                                  </tr>
+                                  <tr>
+                                    <th height="110" bgcolor="#FFFFFF" id="info_10">트리니타스 T2는 드럼 내부에 형성된 열풍의 온도와 생두의 온도를 ㅊㄱ정하여 사용자의 보다 정밀한 로스팅을 지원합니다.</th>
+                                  </tr>
+                                  <tr>
+                                    <th height="110" bgcolor="#FFFFFF" id="info_9"><p>과열 안전장치 : 로스터기의 과열을 감지하면 자동으로 열량을 낮추어 안전한 온도를 유지하는 기능</p>
+                                      <p>유속 안전장치 : 연소에서 배기까지 공기의 흐름에 이상이 감지되면 자동으로 가스를 차단하는 기능</p>
+                                      <p>소화 안전장치 : 버너가 소화되면 자동으로 가스를 차단하는 기능</p></th>
+                                  </tr>
+                                  <tr>
+                                    <th height="54" bgcolor="#FFFFFF" id="info_8">로스팅에 필요한 도구와 생두 등을 수납할 수 있는 전용 테이블이 함께 제공됩니다.</th>
+                                  </tr>
+                                  <tr>
+                                    <th height="54" bgcolor="#FFFFFF" id="info_12">USB 케이블로 아티산 소프트웨어와 연결하여 온도그래프를 확인하여 프로파일을 관리할 수 있습니다.(아티산 소프트웨어는 로스팅을 위한 공개 프로그램입니다.)</th>
+                                  </tr>
+                                </tbody>
+                              </table></th>
+                            </tr>
+                          </tbody>
+                        </table></th>
+                      </tr>
+                      </tbody>
+                    </table></th>
+                  </tr>
+                </tbody>
+          </table>
+             
+          <div class="section" id="spec"><!--.section-->
+                
+       	  <h4><strong>Specifications: TRINITAS T2</strong></h4>
+          <div align="right"><img src="../public_html/inc/img/spec01.jpg" width="500" height="548" alt="T2_Spec"/></div>
+          </div>
+          
+        <strong><!--.section-->  
+             
+        <div class="section" id="T2 gall">
+          <h4><strong>Gallery</strong></h4>
+        <strong><!--.section--> 
+        
+        <table width="788" border="1">
+  <tbody>
+    <tr>
+      <td width="470" rowspan="5"></td>
+      <td width="148"><img src="../public_html/inc/img/thumb_gall_01.JPG" alt="thumb01" width="150" height="150" id="Image1""/></td>
+      <td width="148"><img src="../public_html/inc/img/thumb_gall_02.jpg" alt="thumb02" width="150" height="150" /></td>
+    </tr>
+    <tr>
+      <td><img src="../public_html/inc/img/thumb_gall_03.jpg" alt="thumb03" width="150" height="150" /></td>
+      <td><img src="../public_html/inc/img/thumb_gall_04.jpg" alt="thumb04"width="150" height="150" /></td>
+    </tr>
+    <tr>
+      <td><img src="../public_html/inc/img/thumb_gall_05.jpg" alt="thumb05" width="150" height="150" /></td>
+      <td><img src="../public_html/inc/img/thumb_gall_10.jpg" alt="thumb10" width="150" height="150" /></td>
+    </tr>
+    <tr>
+      <td><img src="../public_html/inc/img/thumb_gall_07.jpg" alt="thumb07" width="150" height="150" /></td>
+      <td><img src="../public_html/inc/img/thumb_gall_08.jpg" alt="thumb08" width="150" height="150" /></td>
+    </tr>
+    <tr>
+      <td><img src="../public_html/inc/img/thumb_gall_11.jpg" alt="thumb11" width="150" height="150" /></td>
+      <td><img src="../public_html/inc/img/thumb_gall_09.jpg" alt="thumb09" width="150" height="150" /></td>
+    </tr>
+  </tbody>
+</table>
+
+        </div>
+
+</div>
+          
+       
+        
+    </div><!--.inner_wrap-->
+  </div>
+
+  <!--#bd-->
+    
+    
+    
+<?
+include_once "../public_html/_ft.php";
+?>
+
+</div><!--.wrap-->
+
+<script type="text/javascript" src="../public_html/inc/js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="../public_html/inc/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="../public_html/inc/js/jquery.hks_layer_popup.js"></script>
+<script type="text/javascript" src="../public_html/inc/js/jquery.hks_form.js"></script>
+<script type="text/javascript" src="../public_html/inc/js/jquery.lazyload.js"></script>
+<script type="text/javascript" src="../public_html/inc/js/common.js"></script>
+<script type="text/javascript">
+$(function(){
+	
+	$('#features dd,#features dt').fadeIn(3000);
+	
+	$("div.section img").lazyload({
+		effect : "fadeIn",
+		effectspeed: 2000
+	});
+	
+	setNav('m4');
+});
+
+</script>
+
+</body>
+</html>
---
+++ keyboard.php
@@ -0,0 +1,9 @@
+<?php
+
+echo <<< EOD
+{
+    "type" : "buttons",
+    "buttons" : ["점화불량", "소음발생", "아티산연결"]
+}
+EOD;
+?>
